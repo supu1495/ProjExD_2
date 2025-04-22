82a835430f32388fa8e12cf9d6203e14af490bfa
c0a242346c <c0a242346c@edu.teu.ac.jp>
2025-04-22 15:07:12 +0900

    当たり判定と終了まで作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -12,22 +13,49 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数:こうかとんRectまたは爆弾Rect
+    戻り値:タプル(横方向判定結果, 縦方向判定結果)
+    画面内ならTrue, 画面外ならFalse
+    """
+    side, tate = True, True #縦,横の変数
+    #横方向判定(画面内だったら)
+    if rct.left < 0 or WIDTH < rct.right:
+        side = False
+    #縦方向判定(画面内だったら)
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return side, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    #爆弾の初期化
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])
 
+        if kk_rct.colliderect(bb_rct):
+            print("Game Over")
+            return
+        
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items(): #DELTAの中身が4つ分for文で回ってる
@@ -37,13 +65,22 @@ def main():
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
-        #     sum_mv[1] += 5
+        #     sum_mv[1] += 5 
         # if key_lst[pg.K_LEFT]:
         #     sum_mv[0] -= 5
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True): #画面外だったら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) #画面内に戻す
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+        side, tate = check_bound(bb_rct)
+        if not side:
+            vx *= -1
+        if not tate:
+            vy *= -1
         pg.display.update()
         tmr += 1
         clock.tick(50)
